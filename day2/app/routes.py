bbec5cc456307c90b353eaf809293023c74e803f
Anotnio <lateralprogramming@gmail.com>
2021-03-03 08:02:42 -0800

    finished... for now

--- day2/app/routes.py
+++ day2/app/routes.py
@@ -1,20 +1,21 @@
 from app import app
 from flask import render_template, request
+from app.forms import UserInfo
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     title = "AJ Book | HOME"
-    return render_template('index.html')
+    return render_template('index.html', title=title)
 
 
-@app.route('/book')
+@app.route('/book', methods=['GET', 'POST'])
 def book():
     title = "AJ Book | BOOK"
-    return render_template('book.html')
+    return render_template('book.html', title=title)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     title = "AJ Book | REGISTER"
     form = UserInfo()
@@ -22,4 +23,4 @@ def register():
         name = form.name.data
         phone = form.phone.data
         address = form.address.data
-    return render_template('register.html')
+    return render_template('register.html', form=form, title=title)
--- day2/config.py
+++ day2/config.py
@@ -3,3 +3,4 @@ import os
 class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
+    SECRET_KEY = 'any type of string'
